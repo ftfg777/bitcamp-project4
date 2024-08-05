e0dd0b771fb93b25bb83264214105e85cb2b6420
ftfg777 <ftfgz5093@gmail.com>
2024-08-05 11:15:38 +0900

    리소스 경로 변경

--- app-common/src/main/java/bitcamp/vo/MP3Player.java
+++ app-common/src/main/java/bitcamp/vo/MP3Player.java
@@ -1,5 +1,6 @@
 package bitcamp.vo;
 
+import java.io.InputStream;
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
 
@@ -9,9 +10,13 @@ import java.io.IOException;
 public class MP3Player {
 
     public static void shot() {
-        String filePath = "app-common/src/main/resources/sound/shot.mp3";
-        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
-            Player player = new Player(fileInputStream);
+        // ClassLoader를 사용하여 리소스 파일에 접근
+        ClassLoader classLoader = MP3Player.class.getClassLoader();
+        try (InputStream inputStream = classLoader.getResourceAsStream("sound/shot.mp3")) {
+            if (inputStream == null) {
+                throw new IllegalArgumentException("MP3 파일을 찾을 수 없습니다.");
+            }
+            Player player = new Player(inputStream);
             player.play();
 
         } catch (JavaLayerException | IOException e) {
@@ -21,9 +26,13 @@ public class MP3Player {
     }
 
     public static void emptyBullet() {
-        String filePath = "app-common/src/main/resources/sound/emptyBullet.mp3";
-        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
-            Player player = new Player(fileInputStream);
+        // ClassLoader를 사용하여 리소스 파일에 접근
+        ClassLoader classLoader = MP3Player.class.getClassLoader();
+        try (InputStream inputStream = classLoader.getResourceAsStream("sound/emptyBullet.mp3")) {
+            if (inputStream == null) {
+                throw new IllegalArgumentException("MP3 파일을 찾을 수 없습니다.");
+            }
+            Player player = new Player(inputStream);
             player.play();
 
         } catch (JavaLayerException | IOException e) {
@@ -33,9 +42,13 @@ public class MP3Player {
     }
 
     public static void loadingBullet() {
-        String filePath = "app-common/src/main/resources/sound/loadingBullet.mp3";
-        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
-            Player player = new Player(fileInputStream);
+        // ClassLoader를 사용하여 리소스 파일에 접근
+        ClassLoader classLoader = MP3Player.class.getClassLoader();
+        try (InputStream inputStream = classLoader.getResourceAsStream("sound/loadingBullet.mp3")) {
+            if (inputStream == null) {
+                throw new IllegalArgumentException("MP3 파일을 찾을 수 없습니다.");
+            }
+            Player player = new Player(inputStream);
             player.play();
 
         } catch (JavaLayerException | IOException e) {
@@ -43,6 +56,42 @@ public class MP3Player {
             e.printStackTrace();
         }
     }
+
+//    public static void shot() {
+//        String filePath = "app-common/src/main/resources/sound/shot.mp3";
+//        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
+//            Player player = new Player(fileInputStream);
+//            player.play();
+//
+//        } catch (JavaLayerException | IOException e) {
+//            System.out.println("MP3 재생 오류: " + e.getMessage());
+//            e.printStackTrace();
+//        }
+//    }
+
+//    public static void emptyBullet() {
+//        String filePath = "app-common/src/main/resources/sound/emptyBullet.mp3";
+//        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
+//            Player player = new Player(fileInputStream);
+//            player.play();
+//
+//        } catch (JavaLayerException | IOException e) {
+//            System.out.println("MP3 재생 오류: " + e.getMessage());
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static void loadingBullet() {
+//        String filePath = "app-common/src/main/resources/sound/loadingBullet.mp3";
+//        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
+//            Player player = new Player(fileInputStream);
+//            player.play();
+//
+//        } catch (JavaLayerException | IOException e) {
+//            System.out.println("MP3 재생 오류: " + e.getMessage());
+//            e.printStackTrace();
+//        }
+//    }
 }
 
 
